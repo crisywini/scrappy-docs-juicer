45c3f90a8b6c8904da408797d08053f9757e4c3f
Cristian Sanchez Pineda <cristian.sanchezp@perficient.com>
2024-03-05 14:03:18 -0500

    Add init struture

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A system that allows to extract web site information and answer questions related to that source',
+    author='Cristian Sánchez Pineda',
+    license='MIT',
+)
